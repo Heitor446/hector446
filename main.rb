9202038d149f330cca423e2a465432f132f5761c
HeitorSouza3 <>
2021-10-29 07:42:12 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,10 @@
+exemplo = {nome: "Rafael", idade:30}
+puts "meu nome é #{exemplo [:nome]} e eu tenho #{exemplo [:idade]} anos"
+
+exemplo = {a: 2}
+puts exemplo [:a]
+
+
+teste = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20]
+ puts "#{teste[:10]}"
+ 
